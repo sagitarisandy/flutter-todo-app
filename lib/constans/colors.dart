fa17e9a7595dea8d0cbdecdc8ee34b864595c7b7
sagitarisandy <97603275+sagitarisandy@users.noreply.github.com>
2024-07-19 15:13:53 +0700

    create search box

--- lib/constans/colors.dart
+++ lib/constans/colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 const Color tdRed = Color(0xFFDA4040);
 const Color tdBlue = Color(0xFF5F52EE);
 
-const Color tdBlack = Color(0xFF3A3A3A);
-const Color tdGrey = Color(0xFF717171);
+const Color tdBlack = Color.fromARGB(255, 82, 82, 82);
+const Color tdGrey = Color.fromARGB(255, 169, 169, 169);
 
-const Color tdBGColor = Color(0xEFEEEFF5);
+const Color tdBGColor = Color.fromARGB(255, 249, 242, 242);
--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:my_flutter_project/screens/home.dart';
 
 void main() {
@@ -11,7 +12,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application
   @override
   Widget build(BuildContext context){
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'ToDo App',
       home: Home(),
     );
